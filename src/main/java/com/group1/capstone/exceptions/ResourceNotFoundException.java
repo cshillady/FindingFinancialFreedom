2ca4ee0540fedc6388279270dd96462c80b5c76b
Connor Shillady <103607820+cshillady@users.noreply.github.com>
2022-08-17 16:54:57 -0400

    Delete src directory

--- src/main/java/com/group1/capstone/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.group1.capstone.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/group1/capstone/models/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.group1.capstone.models;
-
-public enum ERole {
-  ROLE_USER,
-  ROLE_ADMIN
-}
